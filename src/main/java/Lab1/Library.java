561624532d2c9ec019e8df578d5fa96b36c7ec96
yzhangkr <yzhangkr@CCVDIPS6205.ust.hk>
2022-09-18 22:01:48 +0800

    I have made a change to Lab1

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -6,7 +6,9 @@ package Lab1;
 public class Library {
 	
 	public static void main(String arg[]) {
-		System.out.println("Hello, world!");
+		Library lib = new Library();
+		if (lib.someLibraryMethod())
+			System.out.println("Hello, World!");
 		
 	}
 	
